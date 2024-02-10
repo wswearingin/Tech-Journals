0a8074b3982abcf0993859a838c29ac7ec1047c9
William Swearingin <williamtswearingin@gmail.com>
2024-02-10 13:55:15 -0500

    test

--- SEC-335/Scripts/portscanner.sh
+++ SEC-335/Scripts/portscanner.sh
@@ -7,6 +7,6 @@ echo "host,port"
 for host in $(cat $hostfile); do
 	for port in $(cat $hostfile); do
 		timeout .1 bash -c "echo >/dev/tcp/$host/$port" 2>/dev/null &&
-			echo "$host,$port"	
+			echo "$host,$port"
 	done
 done
