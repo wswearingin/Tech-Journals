8a2962f3d25d2b2f13c96008cec2dfbd3c273b44
William Swearingin <williamtswearingin@gmail.com>
2024-02-12 14:36:02 -0500

    test

--- SEC-335/Scripts/portscanner.sh
+++ SEC-335/Scripts/portscanner.sh
@@ -3,13 +3,20 @@
 hostfile=$1
 portfile=$2
 
+while ! [ -f "$hostfile" ]; do
+	read -p "Enter path to hostfile: " $hostfile
+done
+
+while ! [ -f "$portfile" ]; do
+	read -p "Enter path to portfile: " $portfile
+done
+
 echo "host,port"
 
-if [-f $$hostfile]; then
-	for host in $(cat $hostfile); do
-		for port in $(cat $hostfile); do
-			timeout .1 bash -c "echo >/dev/tcp/$host/$port" 2>/dev/null &&
-				echo "$host,$port"
-		done
+
+for host in $(cat $hostfile); do
+	for port in $(cat $hostfile); do
+		timeout .1 bash -c "echo >/dev/tcp/$host/$port" 2>/dev/null &&
+			echo "$host,$port"
 	done
-fi
+done
