fbf1cd8c76d5ddf61874218d9b1934c459ac449d
William Swearingin <williamtswearingin@gmail.com>
2024-02-12 14:35:33 -0500

    added file checking

--- SEC-335/Scripts/portscanner.sh
+++ SEC-335/Scripts/portscanner.sh
@@ -4,9 +4,12 @@ hostfile=$1
 portfile=$2
 
 echo "host,port"
-for host in $(cat $hostfile); do
-	for port in $(cat $hostfile); do
-		timeout .1 bash -c "echo >/dev/tcp/$host/$port" 2>/dev/null &&
-			echo "$host,$port"
+
+if [-f $$hostfile]; then
+	for host in $(cat $hostfile); do
+		for port in $(cat $hostfile); do
+			timeout .1 bash -c "echo >/dev/tcp/$host/$port" 2>/dev/null &&
+				echo "$host,$port"
+		done
 	done
-done
+fi
