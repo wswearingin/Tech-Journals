72a0619b0be7069525d6a796e4e0ac06485d7a1a
William Swearingin <williamtswearingin@gmail.com>
2024-02-20 01:02:02 -0500

    moved null

--- SEC-335/week3/dns-resolver.ps1
+++ SEC-335/week3/dns-resolver.ps1
@@ -4,7 +4,7 @@ Write-Host IP NAME
 
 for ($i=0;$i -lt 255;$i++) {
     $name = Resolve-DnsName -DnsOnly $network'.'$i -Server $server -ErrorAction Ignore | Select -ExpandProperty "NameHost"
-    if ($name -ne $null) {
+    if ($null -ne $name) {
         Write-Host $network'.'$i $name
     }
 }
