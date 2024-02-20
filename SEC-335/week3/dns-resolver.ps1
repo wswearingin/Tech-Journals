11f9a70e2ef4f4190ce475c79601574e4cd6bc2f
William Swearingin <williamtswearingin@gmail.com>
2024-02-20 01:03:13 -0500

    s

--- SEC-335/week3/dns-resolver.ps1
+++ SEC-335/week3/dns-resolver.ps1
@@ -3,7 +3,7 @@ param($network, $server)
 Write-Host IP NAME
 
 for ($i=0;$i -lt 255;$i++) {
-    $name = Resolve-DnsName -DnsOnly $network'.'$i -Server $server -ErrorAction Ignore | Select -ExpandProperty "NameHost"
+    $name = Resolve-DnsName -DnsOnly $network'.'$i -Server $server -ErrorAction Ignore | Select-Object -ExpandProperty "NameHost"
     if ($null -ne $name) {
         Write-Host $network'.'$i $name
     }
