89dc3e028a0f51ac796243c61022f489b8f01005
William Swearingin <williamtswearingin@gmail.com>
2024-02-19 21:13:18 -0500

    yes

--- SEC-335/Scripts/portscanner2.sh
+++ SEC-335/Scripts/portscanner2.sh
@@ -10,7 +10,7 @@ echo "host,port"
 
 for i in {1..254}
 do
-    host=$prefix+"."+$i
+    host=$prefix"."$i
     echo $host
     timeout .1 bash -c "echo >/dev/tcp/$host/$port" 2>/dev/null &&
         echo "$host"
