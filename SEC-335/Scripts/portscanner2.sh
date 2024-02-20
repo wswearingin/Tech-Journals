e3a305eeba50cd755483c62b093bd841e42a0f56
William Swearingin <williamtswearingin@gmail.com>
2024-02-19 21:11:02 -0500

    portscanner2

---
+++ SEC-335/Scripts/portscanner2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Scans entire /24 network for specified port
+
+# Get network prefix and port to scan
+prefix=$1
+port=$2
+
+# Check Ports
+echo "host,port"
+
+for i in {1..254}
+do
+    host=$prefix+"."+$i
+    timeout .1 bash -c "echo >/dev/tcp/$host/$port" 2>/dev/null &&
+        echo "$host"
+done
